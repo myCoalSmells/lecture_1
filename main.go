2ec33354af4f821030da43b46ac78e7e2a830511
Michael Liu <michael.liu.mike@gmail.com>
2023-10-19 23:30:51 -0700

    basic web server

--- main.go
+++ main.go
@@ -1,8 +1,27 @@
 package main
 
-import "net/http"
+import (
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+)
 
 func main() {
+	http.HandleFunc("/", func(rw http.ResponseWriter, r *http.Request) {
+		log.Println("Hello World")
+		d, err := io.ReadAll(r.Body) // reads the request, r, puts it in d
+		if err != nil {
+			http.Error(rw, "oops", http.StatusBadRequest)
+			return
+		}
+		log.Printf("Data %s\n", d)
+		fmt.Fprintf(rw, "Hello %s\n", d)
+	})
+
+	http.HandleFunc("/goodbye", func(http.ResponseWriter, *http.Request) {
+		log.Println("Goodbye World")
+	})
 
 	http.ListenAndServe(":9090", nil)
 }
